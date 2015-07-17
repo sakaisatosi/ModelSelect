0ed734e1301e78f69440c3aae9a70bf2d736d2da
Satosi Sakai <s12369ss@gmail.com>
2015-07-17 16:20:11 +0900

    add getLmModel()

--- R/getModel.R
+++ R/getModel.R
@@ -1,10 +1,19 @@
 #' Get good model
 #' 
-#' @param data
-#'
+#' @param y objective variable
+#' @param x1 first explanatory variable
+#' @param x2 second explanatory variable
+#' 
 #' @return good model
 #'
 #' @export
-getModel <- function(data) {
-  "model"
+getLmModel <- function(y, x1, x2) {
+  model.single <- lm(y ~ x1)
+  model.multi <- lm(y ~ x1 + x2)
+  
+  if (AIC(model.single) > AIC(model.multi)){
+    AIC(model.multi)
+  } else {
+    AIC(model.single)
+  }
 }

97a52956c7549e9800aff8b5743630e203664bec
Satosi Sakai <s12369ss@gmail.com>
2015-12-07 20:50:55 +0900

    一般化線形モデルの正規分布を追加、順番並び替え

--- R/compareModel.R
+++ R/compareModel.R
@@ -44,24 +44,60 @@ compareModel <- function(y, x1) {
     
     # ガンマ分布
     e.gamma <- try(model.gamma <- glm(y ~ x1, family = Gamma))
-    
     if(class(e.gamma) != "try-error"){
       cat("Gamma model's AIC:")
       print(AIC(model.gamma))
     }
     
+    # 正規分布
+    e.gaussian <- try(model.gaussian <- glm(y ~ x1, family = gaussian))
+    if(class(e.gaussian) != "try-error"){
+      cat("Gaussian model's AIC:")
+      print(AIC(model.gaussian))
+    }
+    
   }else if(continuity.flag && !positive.flag){
     cat("応答変数を連続値と判断しました")
-  }else if(!continuity.flag && positive.flag){
-    cat("応答変数を離散値で正の値と判断しました")
-  }else if(!continuity.flag && !positive.flag){
+    
+    # 正規分布
+    e.gaussian <- try(model.gaussian <- glm(y ~ x1, family = gaussian))
+    if(class(e.gaussian) != "try-error"){
+      cat("Gaussian model's AIC:")
+      print(AIC(model.gaussian))
+    }
+    
+  }else if(!continuity.flag){
     cat("応答変数を離散値と判断しました")
+    
+    # ロジスティックモデル
+    e.logistic <- try(model.logistic <- glm(y ~ x1, binomial))
+    if(class(e.logistic) != "try-error"){
+      cat("Logistic model's AIC:")
+      print(AIC(model.logistic))
+    }
+    
+    # ポアソン回帰モデル
+    e.poisson <- try(model.poisson <- glm(y ~ x1, family = poisson))
+    if(class(e.poisson) != "try-error"){
+      cat("Poisson regression model's AIC:")
+      print(AIC(model.poisson))
+    }
   }
   
   # 単回帰
   e.lm <- try(model.lm <- gam(y ~ x1))
+  if(class(e.lm) != "try-error"){
+    cat("Regression model's AIC:")
+    print(AIC(model.lm))
+  }
+  
   # 平滑化スプライン
   e.gam <- try(model.gam <- gam(y ~ s(x1)))
+  if(class(e.gam) != "try-error"){
+    cat("Smoothing spline model's AIC:")
+    print(AIC(model.gam))
+  }
+  
   
   # 累乗モデル
   resid <- function(par){
@@ -74,6 +110,11 @@ compareModel <- function(y, x1) {
                  b = optim(c(1, 1), resid)$par[2])
     )
   )
+  if(class(e.power) != "try-error"){
+    cat("Power model's AIC:")
+    print(AIC(model.power))
+  }
+
   
   # 指数モデル
   resid <- function(par){
@@ -86,16 +127,25 @@ compareModel <- function(y, x1) {
                  b = optim(c(1, 1), resid)$par[2])
     )
   )
+  if(class(e.index) != "try-error"){
+    cat("Exponential model's AIC:")
+    print(AIC(model.index))
+  }
 
   # 漸近指数モデル
   e.asymptotic <- try(model.asymptotic <- nls(y ~ SSasymp(x1, Asymp, RO, lrc)))
+  if(class(e.asymptotic) != "try-error"){
+    cat("Asymptotic exponential model's AIC:")
+    print(AIC(model.asymptotic))
+  }
+  
   # ゴンペルツ成長モデル
   e.gompertz <- try(model.gompertz <- nls(y ~ SSgompertz(x1, Asym, b2, b3)))
-  # ロジスティックモデル
-  e.logistic <- try(model.logistic <- glm(y ~ x1, binomial))
-  # ポアソン回帰モデル
-  e.poisson <- try(model.poisson <- glm(y ~ x1, family = poisson))
-  
+  if(class(e.gompertz) != "try-error"){
+    cat("Gompertz curve model's AIC:")
+    print(AIC(model.gompertz))
+  }
+
   # 遅れS字曲線
   resid <- function(par){
     yhat <- par[1] * (1 - (1 + par[2] * x1) * exp(-par[2] * x1))
@@ -107,6 +157,10 @@ compareModel <- function(y, x1) {
                  b = optim(c(1, 1), resid)$par[2])
     )
   )
+  if(class(e.delay) != "try-error"){
+    cat("Delay S-shaped curve model's AIC:")
+    print(AIC(model.delay))
+  }
   
   # 対数近似
   resid <- function(par){
@@ -119,49 +173,11 @@ compareModel <- function(y, x1) {
                  b = optim(c(1, 25), resid)$par[2])
     )
   )
-  
-  # AICを出力する
-  if(class(e.lm) != "try-error"){
-    cat("Regression model's AIC:")
-    print(AIC(model.lm))
-  }
-  if(class(e.gam) != "try-error"){
-    cat("Smoothing spline model's AIC:")
-    print(AIC(model.gam))
-  }
-  if(class(e.power) != "try-error"){
-    cat("Power model's AIC:")
-    print(AIC(model.power))
-  }
-  if(class(e.index) != "try-error"){
-    cat("Exponential model's AIC:")
-    print(AIC(model.index))
-  }
-  if(class(e.asymptotic) != "try-error"){
-    cat("Asymptotic exponential model's AIC:")
-    print(AIC(model.asymptotic))
-  }
-  if(class(e.gompertz) != "try-error"){
-    cat("Gompertz curve model's AIC:")
-    print(AIC(model.gompertz))
-  }
-  if(class(e.logistic) != "try-error"){
-    cat("Logistic model's AIC:")
-    print(AIC(model.logistic))
-  }
-  if(class(e.poisson) != "try-error"){
-    cat("Poisson regression model's AIC:")
-    print(AIC(model.poisson))
-  }
-  if(class(e.delay) != "try-error"){
-    cat("Delay S-shaped curve model's AIC:")
-    print(AIC(model.delay))
-  }
   if(class(e.logarithm) != "try-error"){
     cat("Logarithmic approximation model's AIC:")
     print(AIC(model.logarithm))
   }
-  
+
   # warningを戻す
   # options(warn = 1)
   # eroorを戻す
